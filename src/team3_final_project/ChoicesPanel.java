92d33188ff8fe32e6fb4f5131d9bf0ebd8804b92
Valeri <Valeri@Valeri-Computer.attlocal.net>
2017-11-10 16:27:09 -0500

    Revert "changed frame size as it was way too large for my screen. couldn't access buttons"
    This reverts commit 7c3e9330cd5b095cd30dc6c6e6eb3146eb505415.

--- src/team3_final_project/ChoicesPanel.java
+++ src/team3_final_project/ChoicesPanel.java
@@ -1,23 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package team3_final_project;
 
-import java.awt.*;
-import javax.swing.*;
-
-public class ChoicesPanel extends JPanel
-{
-    JButton back;
-    
-    public ChoicesPanel()
-    {
-        super();
-        setBackground(Color.lightGray);
-        setLayout(new BorderLayout());
-        
-        JLabel line1 = new JLabel("Testing Choices Panel",SwingConstants.CENTER);
-        add(line1,BorderLayout.CENTER);
-        
-        back = new JButton("Main Menu");
-        add(back,BorderLayout.PAGE_END);
-    }        
+/**
+ *
+ * @author MKrebs
+ */
+public class ChoicesPanel {
     
 }
