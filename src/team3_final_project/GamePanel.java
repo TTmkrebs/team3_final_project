f9932e21595e3163802bbcfe3189cb87375b513b
valer <valer@Valeri-Tablet.attlocal.net>
2017-11-09 21:40:55 -0500

    added image to game panel

---
+++ src/team3_final_project/GamePanel.java
@@ -0,0 +1,28 @@
+package team3_final_project;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel
+{
+    JButton back;
+    ImageIcon campusMap;
+    
+    public GamePanel()
+    {
+        super();
+        setBackground(Color.black);
+        setLayout(new BorderLayout());
+        
+        campusMap = new ImageIcon("images/campus_map_2.jpg"); 
+        
+        JLabel map = new JLabel();
+        map.setIcon(campusMap);
+        
+        JLabel line1 = new JLabel("Testing testing",SwingConstants.CENTER);
+        add(line1,BorderLayout.CENTER);
+        
+        back = new JButton("Main Menu");
+        add(back,BorderLayout.PAGE_END);
+    }
+}
