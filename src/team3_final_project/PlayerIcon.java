6764b0d9741551a0e4281e55e8c28187dd63058d
Valeri <Valeri@Valeri-Computer.attlocal.net>
2017-11-19 22:02:31 -0500

    changed PlayerIcon so new Icon wasn't being created repeatedly

--- src/team3_final_project/PlayerIcon.java
+++ src/team3_final_project/PlayerIcon.java
@@ -6,14 +6,16 @@ import javax.swing.*;
 
 public class PlayerIcon extends JPanel
 {
+    ImageIcon pawPrint;
     public PlayerIcon()
     {
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(100,100));
         
+        pawPrint = new ImageIcon("images/general/paw_print_black.png");
         JLabel pawIcon = new JLabel();
         pawIcon.setLayout(new BorderLayout());
-        pawIcon.setIcon(new ImageIcon("images/general/paw_print_black.png"));
+        pawIcon.setIcon(pawPrint);
         add(pawIcon);
     }
 
